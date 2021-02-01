6c0cbdb716581bb81bc001018c9dadf4b80a615d
MinGyu-Kang <75124028+kagrin97@users.noreply.github.com>
2021-02-01 13:17:46 +0900

    as

--- so.py
+++ so.py
@@ -8,7 +8,6 @@ def get_last_page():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
     pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
-    print(pages)
 
 def get_jobs():
     last_page = get_last_page()
